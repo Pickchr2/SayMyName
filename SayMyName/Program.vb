3356f7c6815f458425330f32d1c6171fee0e3aa4
Chris Pickens <Cpickens1270@gmail.com>
2023-01-22 07:34:37 -0700

    Add project files.

---
+++ SayMyName/Program.vb
@@ -0,0 +1,13 @@
+'Tim Rossiter
+'RCET0265
+'Spring 2020
+'Say My Name
+'https://github.com/pickchr2/SayMyName.git
+
+Imports System
+
+Module Program
+    Sub Main(args As String())
+
+    End Sub
+End Module
